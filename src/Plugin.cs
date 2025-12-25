c74c362cedd71224e4d9d7081f9c6ee74a7fd0dc
Kanayan <glgln6@gmail.com>
2025-12-26 01:45:52 +0900

    v1.0.0

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -19,12 +19,14 @@ internal class Plugin : BaseUnityPlugin
     internal static Plugin? Instance;
     internal ConfigEntry<bool>? EnableRandom;
     internal ConfigEntry<int>? ChoiceCount;
+    internal ConfigEntry<int>? WindowWidth;
 
     private void Awake()
     {
         Instance = this;
-        EnableRandom = Config.Bind("General", "EnableRandom", false, "Enable random gene");
-        ChoiceCount = Config.Bind("General", "ChoiceCount", 20, "The number of choices to display");
+        EnableRandom = Config.Bind("General", "Enable Random", false, "The seed for gene generation will be completely random and not based on character ID. ");
+        ChoiceCount = Config.Bind("General", "Choice Count", 10, "The number of choices to display (Max: 1000)");
+        WindowWidth = Config.Bind("General", "Window Width", 1000, "Width of the selection list (Adjust this if the text overflows.)");
         Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), ModInfo.Guid);
     }
 
