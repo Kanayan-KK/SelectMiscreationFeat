e04ce2e0a30a79ff339d6851def7ea4a319c3f87
Kanayan <glglntn6@gmail.com>
2026-07-04 14:54:36 +0900

    feat: mod有効・無効切り替え、実績解除できない問題を修正

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -10,13 +10,14 @@ public static class ModInfo
 {
     public const string Guid = "SelectMiscreationFeat";
     public const string Name = "Select Miscreation Feat";
-    public const string Version = "1.0.0";
+    public const string Version = "1.0.1";
 }
 
 [BepInPlugin(ModInfo.Guid, ModInfo.Name, ModInfo.Version)]
 internal class Plugin : BaseUnityPlugin
 {
     internal static Plugin? Instance;
+    internal ConfigEntry<bool>? EnableMod;
     internal ConfigEntry<bool>? EnableRandom;
     internal ConfigEntry<int>? ChoiceCount;
     internal ConfigEntry<int>? WindowWidth;
@@ -24,6 +25,7 @@ internal class Plugin : BaseUnityPlugin
     private void Awake()
     {
         Instance = this;
+        EnableMod = Config.Bind("General", "EnableMod", true, "Enable this mod.");
         EnableRandom = Config.Bind("General", "Enable Random", false, "The seed for gene generation will be completely random and not based on character ID. ");
         ChoiceCount = Config.Bind("General", "Choice Count", 10, "The number of choices to display (Max: 1000)");
         WindowWidth = Config.Bind("General", "Window Width", 1000, "Width of the selection list (Adjust this if the text overflows.)");
